d95a0593f1ac9e2365e0ff9eb1d42e38854c5c26
lucasmpalu <lucasmpalu@gmail.com>
2026-03-14 22:54:02 -0300

    fix

--- backend/graph.py
+++ backend/graph.py
@@ -11,8 +11,8 @@ from tavily import TavilyClient
 import graphviz
 import yaml
 from pgvector import PGVector
-#
 
+#
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
 
